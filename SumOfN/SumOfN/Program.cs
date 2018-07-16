fc23dcbf7c4260fd6b2c56ec6913e8b9904839d4
nishanthvrai <nishanthvrai@gmail.com>
2018-07-16 10:13:26 +1000

    Solution Created
    Project Solution Added

---
+++ SumOfN/SumOfN/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Enter N");
+            var N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+                sum = sum + i;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
